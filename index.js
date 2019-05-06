7651ea99cfb5eee575038ede83b3b0e4cda3d8dd
Josh Akeman <joshakeman@gmail.com>
2019-05-06 15:44:16 -0400

    set up initial routes/knex files

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 
 server = express()
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.send("Server it running")
+})
 
 server.listen(5000, () => {
     console.log(" ~~~ Server running on 5k bro ~~~ ")
