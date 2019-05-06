4f8ef732cf71f713083c4c868ee32588e3be51b7
Josh Akeman <joshakeman@gmail.com>
2019-05-06 16:10:08 -0400

    users being created, returning hashed password

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
 
+const Users = require('./users/users-model')
+const db = require('./data/dbConfig')
+
 server = express()
 server.use(express.json())
 
@@ -9,17 +12,21 @@ server.get('/', (req, res) => {
 })
 
 server.post('/api/register', (req, res) => {
-    let user = req.body
-
-    const password = bcrypt.hashSync(user.password, 10)
-
-
-    
-})
-
-function findById(id) {
-    
-}
+    let user = req.body;
+    // check for username and password
+  
+    const hash = bcrypt.hashSync(user.password, 8); // 2^8 rounds <<<<<<<<<<<<
+    // pass > hashit > hash > hashit > hash > hashit > hash
+    user.password = hash; // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
 
 server.listen(5000, () => {
     console.log(" ~~~ Server running on 5k bro ~~~ ")
