dbbbf1c8e240a55c16c885c22a29bd1ea7503fc4
Josh Akeman <joshakeman@gmail.com>
2019-05-06 16:22:41 -0400

    register, login and get working

--- index.js
+++ index.js
@@ -12,12 +12,9 @@ server.get('/', (req, res) => {
 })
 
 server.post('/api/register', (req, res) => {
-    let user = req.body;
-    // check for username and password
-  
-    const hash = bcrypt.hashSync(user.password, 8); // 2^8 rounds <<<<<<<<<<<<
-    // pass > hashit > hash > hashit > hash > hashit > hash
-    user.password = hash; // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    let user = req.body; 
+    const hash = bcrypt.hashSync(user.password, 8);
+    user.password = hash; 
   
     Users.add(user)
       .then(saved => {
@@ -27,6 +24,28 @@ server.post('/api/register', (req, res) => {
         res.status(500).json(error);
       });
   });
+
+  server.post('/api/login', (req, res) => {
+    let { name, password } = req.body
+  
+    Users.findBy({ name })
+      .first()
+      .then(user => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+              res.status(200).json({ message: 'Logged In'})
+          } else {
+              res.status(401).json({ message: 'You shall not pass!'} )
+          }
+      })
+  });
+
+  server.get('/api/users', (req, res) => {
+      Users.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => res.send(err))
+  })
 
 server.listen(5000, () => {
     console.log(" ~~~ Server running on 5k bro ~~~ ")
