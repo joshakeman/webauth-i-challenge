7601a0b32257c019065a8e5c9a633eef40aca0b1
Josh Akeman <joshakeman@gmail.com>
2019-05-06 15:49:46 -0400

    created migration table

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 
 server = express()
 server.use(express.json())
@@ -6,6 +7,19 @@ server.use(express.json())
 server.get('/', (req, res) => {
     res.send("Server it running")
 })
+
+server.post('/api/register', (req, res) => {
+    let user = req.body
+
+    const password = bcrypt.hashSync(user.password, 10)
+
+
+    
+})
+
+function findById(id) {
+    
+}
 
 server.listen(5000, () => {
     console.log(" ~~~ Server running on 5k bro ~~~ ")
